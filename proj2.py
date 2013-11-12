b55a0d2091b02c917d37b7251827d5be3cc7b48e
mskobe <douzongkun@gmail.com>
2013-11-12 17:18:45 -0500

    model 1-first version

--- proj2.py
+++ proj2.py
@@ -1,7 +1,6 @@
 import subprocess
 import requests
 import re
-#from bs4 import BeautifulSoup
 global queries
 
 
@@ -70,9 +69,6 @@ def stopping(query):
 
 
 def stemming(query):
-    f = open('stem-classes.lst', 'r')
-    f2 = open('requests_url', 'w')
-    stemming_list = []
     query_length = 0
     file_address = ' ~/Dropbox/homework/IR-PROJ2/stem-classes.lst'
     for i in range(0, len(query)):
@@ -95,8 +91,10 @@ def stemming(query):
 
 def send_request(query, avg_query_len):
     f = open('requests_url', 'w')
-    #f2 = open('parsed_html', 'w')
+    avg_doclen = get_avg_doclen(3)
     for i in range(0, len(query)):
+        score = {}
+        query_length = len(query[i])
         for j in range(1, len(query[i])):
             make_url = 'http://fiji4.ccs.neu.edu/~zerg/lemurcgi/lemur.cgi?d=3&g=p'
             make_url += '&v=' + query[i][j]
@@ -104,25 +102,55 @@ def send_request(query, avg_query_len):
             f.write('\n')
             r = requests.get(make_url)
             html = r.text
-            parsed_html = re.compile(r'.*?<BODY>(.*?)<HR>',re.DOTALL).match(html).group(1)
-            numbers = re.compile(r'(\d+)',re.DOTALL).findall(parsed_html)
+            parsed_html = re.compile(r'.*?<BODY>(.*?)<HR>', re.DOTALL).match(html).group(1)
+            numbers = re.compile(r'(\d+)', re.DOTALL).findall(parsed_html)
+            #numbers = parse_html(html)
             ctf, df = float(numbers[0]), float(numbers[1])
             inverted_list = map(lambda i: (int(numbers[2 + 3*i]), \
                                            float(numbers[3 + 3*i]),\
                                            float(numbers[4 + 3*i]))\
                                 ,range(0, (len(numbers) - 2)/3))
-            print "ctf= %(ctf)f df= %(df)f" % {'ctf': ctf, 'df':df}
+            #print "ctf= %(ctf)f df= %(df)f" % {'ctf': ctf, 'df':df}
             for (docid,doclen,tf) in inverted_list:
-                print docid,doclen,tf
-        #soup = BeautifulSoup(html)
-        #parsed_html = soup.body
-        #f2.writelines(parsed_html)
-        #f2.write('\n\n')
+                if docid in score:
+                    score[docid] += cal_socre(doclen, tf, avg_doclen) * \
+                                    cal_query_oktf(query_length, tf, avg_query_len)
+                else:
+                    score[docid] = cal_socre(doclen, tf, avg_doclen) * \
+                                   cal_query_oktf(query_length, tf, avg_query_len)
+        sorted_score = sorted(score.iteritems(), key=lambda d:d[1], reverse=True)
+        print sorted_score
     f.close()
-    #f2.close()
+
+
+#def parse_html(html):
+    #parsed_html = re.compile(r'.*?<BODY>(.*?)<HR>', re.DOTALL).match(html).group(1)
+    #numbers = re.compile(r'(\d+)', re.DOTALL).findall(parsed_html)
+    #return numbers
+
+
+def get_avg_doclen(database_num):
+    url = 'http://fiji4.ccs.neu.edu/~zerg/lemurcgi/lemur.cgi?d=?'
+    r = requests.get(url)
+    html = r.text
+    parsed_html = re.compile(r'.*?<BODY>(.*?)<HR>', re.DOTALL).match(html).group(1)
+    numbers = re.compile(r'(\d+)', re.DOTALL).findall(parsed_html)
+    #numbers = parse_html(html)
+    avg_doclen = numbers[5*(database_num-1)+4]
+    return avg_doclen
+
+
+def cal_socre(doclen, tf, avg_doclen):
+    score = float(tf) / (tf+ 0.5 + 1.5*doclen/avg_doclen)
+    return score
+
+
+def cal_query_oktf(query_length, tf, avg_query_len):
+    query_oktf = float(tf) / (tf + 0.5 + 1.5*query_length/avg_query_len)
+    return query_oktf
 
 
 queries = get_query()
 queries_after_stopping = stopping(queries)
 queries_after_stemming, avg_query_len = stemming(queries_after_stopping)
-#send_request(queries_after_stemming, avg_query_len)
+send_request(queries_after_stemming, avg_query_len)
